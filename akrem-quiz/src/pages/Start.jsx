075118be657a70b526e7ad42f86a4018a52c3d09
mouheb htiwch <93223049+Mouuheb@users.noreply.github.com>
2025-03-09 12:15:54 +0100

    quiz

---
+++ akrem-quiz/src/pages/Start.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './start.css'
+
+const Start = () => {
+  return (
+    <div className='start-page' >
+        <div className='kk'>
+            
+        </div>
+        <div className='text-cnt'>
+            <h1>Welcome</h1>
+            <h1>to Akrem Quiz</h1>
+            <div className='text'>
+                <p>Play,Learn,and Explore with Exciting</p>
+                <p>Quizzes!</p>
+            </div>
+            
+            <div className='btn'>
+                <h3>GET STARTED</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Start
